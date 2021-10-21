6f904e5d5763d591a72fbb5d0433d063914d94af
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-21 12:21:05 +0200

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/retopromethus/RetoPromethusApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.retopromethus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoPromethusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoPromethusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/retopromethus/RetoPromethusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.retopromethus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoPromethusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
